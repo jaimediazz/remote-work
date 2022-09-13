e59cb2091e0c95712d05b823708a141d7936391c
Jaime <jaime99dr@gmail.com>
2022-09-13 11:06:40 +0100

    responsive menu open/close done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainContainerComponent } from './components/main-container/main-container.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ResponsiveMenuComponent } from './components/responsive-menu/responsive-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainContainerComponent,
-    NavBarComponent
+    NavBarComponent,
+    ResponsiveMenuComponent
   ],
   imports: [
     BrowserModule,
